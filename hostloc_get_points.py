5f0469541e51bcc15fed57742ed592e467f14e89
Ming Kuang <ming@inkuang.com>
2020-12-01 22:15:02 +0800

    add the function of automatically passing the anti-CC mechanism

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -1,9 +1,12 @@
 import os
-import requests
-from requests import Session as req_Session
 import time
 import random
 import re
+import textwrap
+import requests
+
+from pyaes import AESModeOfOperationCBC
+from requests import Session as req_Session
 
 
 # 随机生成用户空间链接
@@ -15,6 +18,64 @@ def randomly_gen_uspace_url() -> list:
         url = "https://www.hostloc.com/space-uid-{}.html".format(str(uid))
         url_list.append(url)
     return url_list
+
+
+# 使用Python实现的防CC验证页面中JS写的的toNumbers函数
+def toNumbers(secret):
+    text = []
+    for value in textwrap.wrap(secret, 2):
+        text.append(int(value, 16))
+    return text
+
+
+# 不带Cookies访问论坛首页，检查是否开启了防CC机制，将开启状态、AES计算所需的参数全部放在一个字典中返回
+def check_anti_cc() -> dict:
+    result_dict = {}
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    }
+    home_page = "https://www.hostloc.com/forum.php"
+    res = requests.get(home_page, headers=headers)
+    aes_keys = re.findall('toNumbers\("(.*?)"\)', res.text)
+    cookie_name = re.findall('cookie="(.*?)="', res.text)
+
+    if len(aes_keys) != 0:  # 开启了防CC机制
+        print("检测到防 CC 机制开启！")
+        if len(aes_keys) != 3 or len(cookie_name) != 1:  # 正则表达式匹配到的参数数量不对（不正常的情况）
+            result_dict["ok"] = 0
+        else:  # 匹配正常时将参数存到result_dict中
+            result_dict["ok"] = 1
+            result_dict["cookie_name"] = cookie_name[0]
+            result_dict["a"] = aes_keys[0]
+            result_dict["b"] = aes_keys[1]
+            result_dict["c"] = aes_keys[2]
+    else:
+        pass
+
+    return result_dict
+
+
+# 在开启了防CC机制时使用获取到的数据进行AES解密计算生成一条Cookie（未开启时返回空Cookies）
+def gen_anti_cc_cookies() -> dict:
+    cookies = {}
+    anti_cc_status = check_anti_cc()
+
+    if not anti_cc_status:  # 未开启防CC机制
+        pass
+    elif anti_cc_status["ok"] == 0:
+        print("防 CC 验证过程所需参数不符合要求，页面可能存在错误！")
+    else:  # 使用获取到的三个值进行AES Cipher-Block Chaining解密计算以生成特定的Cookie值用于通过防CC验证
+        print("执行 AES 计算尝试通过防 CC 验证")
+        a = bytes(toNumbers(anti_cc_status["a"]))
+        b = bytes(toNumbers(anti_cc_status["b"]))
+        c = bytes(toNumbers(anti_cc_status["c"]))
+        cbc_mode = AESModeOfOperationCBC(a, b)
+        result = cbc_mode.decrypt(c)
+
+        name = anti_cc_status["cookie_name"]
+        cookies[name] = result.hex()
+
+    return cookies
 
 
 # 登录帐户
@@ -32,8 +93,11 @@ def login(username: str, password: str) -> req_Session:
         "quickforward": "yes",
         "handlekey": "ls",
     }
+
     s = req_Session()
-    res = s.post(url=login_url, data=login_data, headers=headers)
+    s.headers.update(headers)
+    s.cookies.update(gen_anti_cc_cookies())
+    res = s.post(url=login_url, data=login_data)
     res.raise_for_status()
     return s
 
@@ -45,6 +109,7 @@ def check_login_status(s: req_Session, number_c: int) -> bool:
     res.raise_for_status()
     res.encoding = "utf-8"
     test_title = re.findall("<title>(.*?)<\/title>", res.text)
+
     if len(test_title) != 0:  # 确保正则匹配到了内容，防止出现数组索引越界的情况
         if test_title[0] != "个人资料 -  全球主机交流论坛 -  Powered by Discuz!":
             print("第", number_c, "个帐户登录失败！")
@@ -53,7 +118,7 @@ def check_login_status(s: req_Session, number_c: int) -> bool:
             print("第", number_c, "个帐户登录成功！")
             return True
     else:
-        print("未在用户设置页面找到标题，该页面可能存在错误！")
+        print("无法在用户设置页面找到标题，该页面存在错误或被防 CC 机制拦截！")
         return False
 
 
@@ -64,6 +129,7 @@ def print_current_points(s: req_Session):
     res.raise_for_status()
     res.encoding = "utf-8"
     points = re.findall("积分: (\d+)", res.text)
+
     if len(points) != 0:  # 确保正则匹配到了内容，防止出现数组索引越界的情况
         print("帐户当前积分：" + points[0])
     else:
@@ -83,7 +149,7 @@ def get_points(s: req_Session, number_c: int):
                 res = s.get(url)
                 res.raise_for_status()
                 print("第", i + 1, "个用户空间链接访问成功")
-                time.sleep(5)  # 每访问一个链接后休眠5秒，以避免触发论坛的防cc机制
+                time.sleep(5)  # 每访问一个链接后休眠5秒，以避免触发论坛的防CC机制
             except Exception as e:
                 print("链接访问异常：" + str(e))
             continue
